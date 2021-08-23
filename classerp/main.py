89f5e6509e1a47c386214fb61eeb1885e795fb6f
neethu <neethu@gmail.com>
2021-08-23 21:41:51 +0530

    Modifying erp project using class

--- classerp/main.py
+++ classerp/main.py
@@ -1,7 +1,7 @@
 from employee import Emp
 from empstore import emp
-
-
+import change_employee as cemp
+import search_employee as semp
 
 def main_menu():
     print("1. Add employee")
@@ -36,74 +36,6 @@ def delete_employee():
         pass
 
 
-def search_employee_menu():
-    print("\t1.search by name ")
-    print("\t2.search by age ")
-    print("\t3.search by salary ")
-    print("\t4.search by gender ")
-    print("\t5.exit ")
-
-
-def serach_by_name():
-    name = input("Enter name :")
-    emp_l = list(filter(lambda a: a.name == name, emp))
-    if len(emp) == 0:  # not st #st
-        print("No employee found")
-    else:
-        for i in emp:
-            print(
-                f'{i.name} |{i.age} | {i.gender}|{i.place} |{i.salary} | {i.previous_company} | {i.joining_date} ')
-
-
-def search_by_age():
-    age = input("Enter age :")
-    emp_l = list(filter(lambda a: a.age == age, emp))
-    if len(emp) == 0:  # not st #st
-        print("No employee found")
-    else:
-        for i in emp:
-            print(
-                f'{i.name} |{i.age} | {i.gender}|{i.place} |{i.salary} | {i.previous_company} | {i.joining_date} ')
-
-
-def search_by_salary():
-    salary = input("Enter salary :")
-    emp_l = list(filter(lambda a: a.salary == salary, emp))
-    if len(emp) == 0:  # not st #st
-        print("No employee found")
-    else:
-        for i in emp:
-            print(
-                f'{i.name} |{i.age} | {i.gender}|{i.place} |{i.salary} | {i.previous_company} | {i.joining_date} ')
-
-
-def search_by_gender():
-    gender = input("Enter gender :")
-    emp_l = list(filter(lambda a: a.gender == gender, emp))
-    if len(emp) == 0:  # not st #st
-        print("No employee found")
-    else:
-        for i in emp:
-            print(
-                f'{i.name} |{i.age} | {i.gender}|{i.place} |{i.salary} | {i.previous_company} | {i.joining_date} ')
-
-
-def search_employee():
-
-    while True:
-        search_employee_menu()
-        ch = int(input("Enter choice : "))
-        if ch == 1:
-            serach_by_name()
-        elif ch == 2:
-            search_by_age()
-        elif ch == 3:
-            search_by_salary()
-        elif ch == 4:
-            search_by_gender()
-        elif ch == 5:
-            break
-
 
 def display_employee():
     for i in emp:
@@ -111,8 +43,6 @@ def display_employee():
             f'{i.name} |{i.age} | {i.gender}|{i.place} |{i.salary} | {i.previous_company} | {i.joining_date} ')
 
 
-def change_emp_data():
-    pass
 
 
 while True:
@@ -128,11 +58,11 @@ while True:
 
     elif ch == 3:
         # search employee
-        search_employee()
+        semp.search_employee()
     elif ch == 4:
         display_employee()
     elif ch == 5:
-        change_emp_data()
+        cemp.change_emp_data()
     elif ch == 6:
         break
     else:
